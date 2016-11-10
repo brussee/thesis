7a36de838a1a80ecf9ade4646f268da4147a5bfd
Paul Brussee <brussee@live.nl>
2016-11-10 18:34:24 +0100

    copy api benchmark r scripts and jmeter config from other repo

---
+++ api_benchmark/r/install.r
@@ -0,0 +1,8 @@
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+
+toInstall <- c("ggplot2", "reshape2", "reshape", "stringr", "plyr", "foreach", "data.table", "gridExtra")
+for (package in toInstall){
+	if (is.installed(package) == FALSE){
+		install.packages(package, repos = "http://cran.r-project.org")
+	}
+}
